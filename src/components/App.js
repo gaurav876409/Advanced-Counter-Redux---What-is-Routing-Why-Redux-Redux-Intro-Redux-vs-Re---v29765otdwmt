6cdc58a14978a024985270ee6d6e270db4f9db1e
gaurav876409 <gaurav876409@gmail.com>
2023-06-16 12:14:10 +0530

    up

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, signIn, signOut, incrementByAmount } from '../actions/action';
 
-function App() {
+const App = () => {
+  const [amount, setAmount] = useState(2);
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
+  const handleIncrementByAmount = () => {
+    dispatch(incrementByAmount(parseInt(amount)));
+  };
+
+  const handleLogin = () => {
+    dispatch(signIn());
+  };
+
+  const handleLogout = () => {
+    dispatch(signOut());
+  };
 
   return (
-    <div id='main'>
-      
+    <div>
+      {isLogged ? (
+        <>
+          <button onClick={handleLogout}>Logout</button>
+          <button onClick={handleIncrement}>+</button>
+          <span data-testid="counter">{counter}</span>
+          <button onClick={handleDecrement}>-</button>
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          <button onClick={handleIncrementByAmount}>Add amount</button>
+        </>
+      ) : (
+        <button onClick={handleLogin}>Login</button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
+
--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ import App from './components/App';
 //   document.getElementById('root')
 // );
 
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
